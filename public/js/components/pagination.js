574d9790504c7fae2e4a51138de1c387f7fc5225
damidj92 <damidj92@gmail.com>
2021-10-11 15:10:39 -0300

    Se modificó la funcionalidad del paginado y su diseño

--- public/js/components/pagination.js
+++ public/js/components/pagination.js
@@ -13,43 +13,92 @@ const sortPagination = (pageActualString, lastPage) => {
     const ul = document.createElement("ul");
     ul.className = "pagination";
 
+    const inicio = pageActual == 1;
     const fin = pageActual >= lastPage;
+    const diferencia = lastPage - pageActual;
+
+    const liAnterior = document.createElement("li");
+    const aAnterior = document.createElement("a");
+    aAnterior.setAttribute("href", newHREF(pageActual - 1));
+    aAnterior.innerHTML = "<";
+    liAnterior.append(aAnterior);
+    inicio ? liAnterior.style.display = "none" : liAnterior.style.display = "inline"
+
+    const liInicio = document.createElement("li");
+    const a = document.createElement("a");
+    a.setAttribute("href", newHREF(1));
+    a.innerHTML = 1;
+    liInicio.append(a);
+    
+    const liSpanInicio = document.createElement("li");
+    const spanInicio = document.createElement("span");
+    spanInicio.className = "spanPuntos";
+    spanInicio.innerHTML = "...";
+    liSpanInicio.append(spanInicio);
+    pageActual > 3 ? liSpanInicio.style.display = "inline" : liSpanInicio.style.display = "none";
 
     const li1 = document.createElement("li");
     const a1 = document.createElement("a");
-    a1.className = "active";
-
-    a1.setAttribute("href", newHREF(pageActual))
-    a1.innerHTML = pageActual;
+    let a1Page = pageActual - 1;
+    a1.setAttribute("href", newHREF(a1Page))
+    a1.innerHTML = a1Page;
     li1.append(a1);
-
+    pageActual <= 2 ? li1.style.display = "none" : li1.style.display = "inline"
+    
     const li2 = document.createElement("li");
     const a2 = document.createElement("a");
-    let a2Page = fin ? pageActual - 1 : pageActual + 1;
-    a2.setAttribute("href", newHREF(a2Page))
-    a2.innerHTML = a2Page;
+    a2.setAttribute("href", newHREF(pageActual))
+    a2.innerHTML = pageActual;
     li2.append(a2);
+    inicio ? li2.style.display = "none" : li2.style.display = "inline"
+    
+    if(inicio){
+        a.className = 'active';
+        li1.style.display = 'none';
+    } else {
+        a2.className = 'active';
+    }
 
     const li3 = document.createElement("li");
     const a3 = document.createElement("a");
-    let a3Page = fin ? pageActual - 2 : pageActual + 2;
+    let a3Page = fin ? pageActual - 1 : pageActual + 1;
     a3.setAttribute("href", newHREF(a3Page))
     a3.innerHTML = a3Page;
     li3.append(a3);
 
+    const liSpanFinal = document.createElement("li");
+    const spanFinal = document.createElement("span");
+    spanFinal.className = "spanPuntos";
+    spanFinal.innerHTML = "...";
+    liSpanFinal.append(spanFinal);
+    fin ? liSpanFinal.style.display = "none" : liSpanFinal.style.display = "inline";
+
     const li4 = document.createElement("li");
-    const span = document.createElement("span")
-    span.innerHTML = "...";
-    li4.append(span);
+    const a4 = document.createElement("a");
+    let a4Page = fin ? 1 : lastPage;
+    a4.setAttribute("href", newHREF(a4Page))
+    a4.innerHTML = a4Page;
+    li4.append(a4);
 
-    const li5 = document.createElement("li");
-    const a5 = document.createElement("a");
-    let a5Page = fin ? 1 : lastPage;
-    a5.setAttribute("href", newHREF(a5Page))
-    a5.innerHTML = a5Page;
-    li5.append(a5);
+    if(!fin && diferencia == 1){
+        li3.style.display = "none";
+        liSpanFinal.style.display = "none";
+    }
 
-    fin ? ul.append(li5, li2, li3, li4, li1) : ul.append(li1, li2, li3, li4, li5)
+    if(!fin && diferencia == 2){
+        liSpanFinal.style.display = "none";
+    }
+
+    const liSiguiente = document.createElement("li");
+    const aSiguiente = document.createElement("a");
+    aSiguiente.setAttribute("href", newHREF(pageActual + 1));
+    aSiguiente.innerHTML = ">";
+    liSiguiente.append(aSiguiente);
+    fin ? liSiguiente.style.display = "none" : liSiguiente.style.display = "inline"
+
+    fin 
+        ? ul.append(liAnterior, li4, liSpanInicio, li3, li2)
+        : ul.append(liAnterior, liInicio, liSpanInicio, li1, li2, li3, liSpanFinal, li4, liSiguiente)
 
     return ul;
 }
